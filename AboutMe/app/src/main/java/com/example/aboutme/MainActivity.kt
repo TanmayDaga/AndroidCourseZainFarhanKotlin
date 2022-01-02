b63f61b8aa757ce98c749d66d2c9a2a6ac80fe4e
Tanmay Daga <tanmay06daga@gmail.com>
2022-01-02 09:19:23 +0530

    see video 24 again

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mBinding: ActivityMainBinding
+    private var myName:MyName = MyName("Tanmay")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding.myName = myName
+
+
+        mBinding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+
+
     }
+
+    private fun addNickName(view: View) {
+
+        mBinding.apply {
+            myName?.nickname = nickNameEditText.text.toString()
+            invalidateAll()
+            nickNameEditText.visibility = View.GONE
+            view.visibility = View.GONE
+            nickNameTextView.visibility = View.VISIBLE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
 }
