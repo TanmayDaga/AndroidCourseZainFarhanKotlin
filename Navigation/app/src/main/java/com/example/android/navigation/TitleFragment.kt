e124d815c1cb17e3dc7eefa877508e1bb5bd4c24
Tanmay Daga <tanmay06daga@gmail.com>
2022-01-03 15:57:11 +0530

    from video 49 4:14

--- Navigation/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Navigation/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -19,6 +21,10 @@ class TitleFragment : Fragment() {
             inflater,R.layout.fragment_title,container,false
         )
 
+
+        binding.playButton.setOnClickListener {
+            view:View->Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 
